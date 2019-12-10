92aa447aa7e9bcc1a9b7a7f49215e04f7668dbe9
filixed <filixed@gmail.com>
2019-12-10 15:20:23 +0100

    added to the first button play and stop song

--- app/src/main/java/com/example/musicapphw/MainActivity.java
+++ app/src/main/java/com/example/musicapphw/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.musicapphw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button =   this.findViewById(R.id.button);
+        final MediaPlayer[] mp = {MediaPlayer.create(this, R.raw.sample1)};
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(mp[0].isPlaying()){
+                    mp[0].stop();
+                    mp[0] = MediaPlayer.create(getApplicationContext(),R.raw.sample1);
+
+                }
+                else mp[0].start();
+
+            }
+        });
+
+
     }
+
+
+
 }
